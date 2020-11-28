d3591777cfff7306cc6f465f6196c8564d50b9ee
steve1303 <52918316+steve1303@users.noreply.github.com>
2020-11-28 14:24:30 +0000

    Update scoreboard.py

--- alien_invasion/scoreboard.py
+++ alien_invasion/scoreboard.py
@@ -18,9 +18,12 @@ class Scoreboard:
 
         # 显示得分信息时使用的字体设置
         self.text_color = (30, 30, 30)
-        self.font = pygame.font.SysFont(None, 48) # 从系统字体库创建一个 Font 对象
+        self.font = pygame.font.SysFont(None, 48)  # 从系统字体库创建一个 Font 对象
 
-        # 准备初始化最高得分和当前得分图像 调用了四个函数
+        self.prep_images()  # 调用下面的函数 (代码的重构)
+
+    def prep_images(self):
+        """绘制最高得分，当前得分,飞船数量，当前等级这四个图像"""
         self.prep_ships()
         self.prep_score()
         self.prep_high_score()
@@ -28,36 +31,37 @@ class Scoreboard:
 
     def prep_ships(self):
         """显示还余下多少艘飞船"""
-        self.ships = Group()
+        self.ships = Group()  # 创建一个空编组
         for ship_number in range(self.stats.ships_left):
             ship = Ship(self.ai_game)
             ship.rect.x = 10 + ship_number * ship.rect.width
-            ship.rect.y = 10
-            self.ships.add(ship)
+            ship.rect.y = 10  # 距离顶部10px
+            self.ships.add(ship)  # 添加到编组ships
 
     # Pygame通过将你要显示的字符串渲染为图像来处理文本。
     def prep_score(self):
         """将当前得分转化为渲染的图像"""
-        rounded_score = round(self.stats.score, -1)
+        rounded_score = round(self.stats.score, -1)  # round() 方法返回浮点数x的四舍五入值
         score_str = "{:,}".format(rounded_score)  # 字符类型的文字
         # score_str = str(self.stats.score)
+
         # 方法font.render() 还接受一个布尔实参，该实参指定开启还是关闭反锯齿功能(反锯齿让文本的边缘更平滑)。
         # 余下的两个实参分别是文本颜色和背景色。
         # 我们启用了反锯齿功能，并将文本的背景色设置为按钮的颜色(如果没有指定背景色，Pygame将以透明背景的方式渲染文本
-        self.score_image = self.font.render(f"Current score: {score_str}", True,
+        self.score_image = self.font.render(f"Score: {score_str}", True,
                     self.text_color, self.settings.bg_color)
 
         # 在屏幕右上角显示得分
-        self.score_rect = self.score_image.get_rect()
-        self.score_rect.right = self.screen_rect.right - 20
-        self.score_rect.top = 20
+        self.score_rect = self.score_image.get_rect()  # 获取分数对象的矩形
+        self.score_rect.right = self.screen_rect.right - 20  # 离右边的距离为20px
+        self.score_rect.top = 20  # 离顶部的距离为20px
 
     def prep_high_score(self):
         """将最高得分转化为渲染的图像"""
-        high_score = round(self.stats.high_score, -1)
+        high_score = round(self.stats.high_score, -1)  # 让小数精确到小数点后的某一位  实参指定为-1 10的整数倍
         high_score_str = "{:,}".format(high_score)
         # score_str = str(self.stats.score)
-        self.high_score_image = self.font.render(f"High score: {high_score_str}", True,
+        self.high_score_image = self.font.render(f"High Score: {high_score_str}", True,
                         self.text_color, self.settings.bg_color)
 
         # 在屏幕顶部中央显示得分
@@ -74,7 +78,7 @@ class Scoreboard:
         # 将等级放在得分的下方
         self.level_rect = self.level_image.get_rect()
         self.level_rect.right = self.score_rect.right
-        self.level_rect.top = self.score_rect.bottom + 10
+        self.level_rect.top = self.score_rect.bottom + 10  # 等级在当前得分底端位置 + 10
 
     def show_score(self):
         """在屏幕上显示得分，等级和余下的飞船数"""
